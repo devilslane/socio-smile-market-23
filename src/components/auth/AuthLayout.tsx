3a08bb85f1a765759a0da65ad28bd7dde595caa7
devilslane <115607690+devilslane@users.noreply.github.com>
2025-04-10 19:26:57 +0530

    Update AuthLayout.tsx

--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -21,7 +21,7 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
             <div className="text-center mb-6">
               <Link to="/" className="inline-block">
                 <img 
-                  src="/logo.png" 
+                  src="/logo1.png" 
                   alt="SocioDent Logo" 
                   className="h-12 mx-auto object-contain"
                 />
